2bade8facf520e6bc5f07468bd01f7dd8dd9cd7d
Jeremías Aragón Bonilla <jerejere2204@gmail.com>
2022-03-05 13:53:56 -0500

    Commit inicial

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {WelcomePageComponent} from "./welcome-page/welcome-page.component";
+import {SurveyPageComponent} from "./survey-page/survey-page.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'inicio',
+    component: WelcomePageComponent
+  },
+  {
+    path: 'encuesta',
+    component: SurveyPageComponent
+  },
+  { path: '**', redirectTo: 'inicio', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
