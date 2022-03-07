1d0bffd8cd4c63df7712f39b64cfcf04d059808a
JeremiasAragon <jerejere2204@gmail.com>
2022-03-06 20:11:00 -0500

    Formulario de encuesta listo para integrar con firebase

--- src/app/survey-page/survey-page.component.ts
+++ src/app/survey-page/survey-page.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import * as answers from './answers.data';
-import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+import {AbstractControl, FormBuilder, FormGroup, Validators} from "@angular/forms";
+import Swal from 'sweetalert2'
 
 @Component({
   selector: 'app-survey-page',
@@ -9,10 +10,27 @@ import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 })
 export class SurveyPageComponent implements OnInit {
 
-  form!: FormGroup;
+  form!: any;
   zonas = answers.zonas;
   dropdownOptions = answers;
   ciudadesFiltradas: any[] = [];
+
+  get zona(): AbstractControl { return this.form.get('zona'); }
+  get edad(): AbstractControl { return this.form.get('edad'); }
+  get ciudad(): AbstractControl { return this.form.get('ciudad'); }
+  get esApartado(): AbstractControl { return this.form.get('esApartado'); }
+  get sabeCantar(): AbstractControl { return this.form.get('sabeCantar'); }
+  get esBautizado(): AbstractControl { return this.form.get('esBautizado'); }
+  get estadoCivil(): AbstractControl { return this.form.get('estadoCivil'); }
+  get viveConPadres(): AbstractControl { return this.form.get('viveConPadres'); }
+  get nivelEducativo(): AbstractControl { return this.form.get('nivelEducativo'); }
+  get nombreCompleto(): AbstractControl { return this.form.get('nombreCompleto'); }
+  get estaEstudiando(): AbstractControl { return this.form.get('estaEstudiando'); }
+  get fechaNacimiento(): AbstractControl { return this.form.get('fechaNacimiento'); }
+  get tocaInstrumentos(): AbstractControl { return this.form.get('tocaInstrumentos'); }
+  get sirveEnDirectiva(): AbstractControl { return this.form.get('sirveEnDirectiva'); }
+  get recibioEspirituSanto(): AbstractControl { return this.form.get('recibioEspirituSanto'); }
+
 
   constructor(private formBuilder: FormBuilder) { }
 
@@ -21,7 +39,7 @@ export class SurveyPageComponent implements OnInit {
       nombreCompleto: ['', Validators.required],
       fechaNacimiento: ['', Validators.required],
       edad: [null, Validators.required],
-      telefono: [null, Validators.required],
+      telefono: [null],
       viveConPadres: [null, Validators.required],
       estadoCivil: ['', Validators.required],
       zona: ['', Validators.required],
@@ -40,8 +58,28 @@ export class SurveyPageComponent implements OnInit {
     });
 
     this.form.controls['fechaNacimiento'].valueChanges
-      .subscribe((value) => {
+      .subscribe((value: any) => {
         this.form.controls['edad'].setValue(this.calculateAge(value));
+      });
+
+    this.form.controls['sirveEnDirectiva'].valueChanges
+      .subscribe((value: any) => {
+        if(!value) {
+          this.form.controls['directivas'].setValue('');
+          this.form.controls['directivas'].disable();
+        } else {
+          this.form.controls['directivas'].enable();
+        }
+      });
+
+    this.form.controls['tocaInstrumentos'].valueChanges
+      .subscribe((value: any) => {
+        if(!value) {
+          this.form.controls['instrumentos'].setValue('');
+          this.form.controls['instrumentos'].disable();
+        } else {
+          this.form.controls['instrumentos'].enable();
+        }
       });
   }
 
@@ -72,6 +110,26 @@ export class SurveyPageComponent implements OnInit {
   }
 
   sendData() {
+    if(this.form.invalid) {
+      Swal.fire({
+        title: '¡Error!',
+        text: 'Verifica la información faltante e intenta nuevamente',
+        icon: 'error',
+        confirmButtonText: 'Entendido'
+      });
+
+      return;
+    }
+
     console.log(this.form.value);
+
+    Swal.fire({
+      title: '¡Listo!',
+      text: 'Tu información ha sido registrada con éxito',
+      icon: 'success',
+      confirmButtonText: 'Entendido'
+    });
+
+    this.form.reset();
   }
 }
